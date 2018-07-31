bcc1052bbd51c781f16a9d708df3df923cd32da8
Andrew Greenwood <andrew@greenwoodprojects.net>
2018-07-31 03:09:27 +0100

    Simulate ISA bus reads/writes on PC.

---
+++ prototype/ISABus.h
@@ -0,0 +1,29 @@
+/*
+    Project:    Canyon
+    Purpose:    Simulated ISA Bus Interface (for prototyping embedded code)
+    Author:     Andrew Greenwood
+    Date:       July 2018
+*/
+
+#ifndef CANYON_SIMULATED_ISABUS_H
+#define CANYON_SIMULATED_ISABUS_H 1
+
+#include <stdint.h>
+
+class ISABus {
+    public:
+        ISABus();
+
+        void reset() const;
+
+        void write(
+            uint16_t address,
+            uint8_t data) const;
+
+        uint8_t read(
+            uint16_t address) const;
+
+    private:
+};
+
+#endif
